e1148d1a43cd8e8ea735e96b4458809efd7e24e9
Malibu <matthieu-p93@live.fr>
2015-02-13 00:59:13 +0100

    template referent

--- SRC/resources/views/admin.blade.php
+++ SRC/resources/views/admin.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 
 <br>
-<center><h1>Bienvenue dans l'espace d'administration!</h1>
-<br>
+<center><h1>Bienvenue dans l'espace d'administration!</h1></center>
+<br><br><br>
 
-<div class="row">
+
+  <div class="col-md-2"></div>
 
   <div class="col-md-8">
+    <button style="float: right" type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Ajouter un référent</button>
     <table class="table table-hover">
       <thead>
         <tr>
@@ -23,42 +25,107 @@
           <td>Quentin Rouland</td>
           <td>quentin.rouland@free.fr</td>
           <td>Toulouse</td>
-          <td>-- Action --</td>
+          <td>
+            <button type="button" class="btn btn-default btn-sm">Se connecter</button>
+            <button type="button" class="btn btn-warning btn-sm">Supprimer</button>
+          </td>
         </tr>
         <tr>
           <td>Quentin Rouland</td>
           <td>quentin.rouland@free.fr</td>
           <td>Toulouse</td>
-          <td>-- Action --</td>
+          <td>
+            <button type="button" class="btn btn-default btn-sm">Se connecter</button>
+            <button type="button" class="btn btn-warning btn-sm">Supprimer</button>
+          </td>
         </tr>
         <tr>
           <td>Quentin Rouland</td>
           <td>quentin.rouland@free.fr</td>
           <td>Toulouse</td>
-          <td>-- Action --</td>
+          <td>
+            <button type="button" class="btn btn-default btn-sm">Se connecter</button>
+            <button type="button" class="btn btn-warning btn-sm">Supprimer</button>
+          </td>
         </tr>
         <tr>
           <td>Quentin Rouland</td>
           <td>quentin.rouland@free.fr</td>
           <td>Toulouse</td>
-          <td>-- Action --</td>
+          <td>
+            <button type="button" class="btn btn-default btn-sm">Se connecter</button>
+            <button type="button" class="btn btn-warning btn-sm">Supprimer</button>
+          </td>
         </tr>
         <tr>
           <td>Quentin Rouland</td>
           <td>quentin.rouland@free.fr</td>
           <td>Toulouse</td>
-          <td>-- Action --</td>
+          <td>
+            <button type="button" class="btn btn-default btn-sm">Se connecter</button>
+            <button type="button" class="btn btn-warning btn-sm">Supprimer</button>
+          </td>
         </tr>
         <tr>
           <td>Quentin Rouland</td>
           <td>quentin.rouland@free.fr</td>
           <td>Toulouse</td>
-          <td>-- Action --</td>
+          <td>
+            <button type="button" class="btn btn-default btn-sm">Se connecter</button>
+            <button type="button" class="btn btn-warning btn-sm">Supprimer</button>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
-  
-  <div class="col-md-4">.col-md-4</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Créer un nouvel adhérent</h4>
+      </div>
+      <div class="modal-body">
+
+          <form class="form-horizontal">
+            <div class="form-group">
+              <label for="prenom" class="col-sm-2 control-label">Prénom</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="prenom" placeholder="Prénom">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="nom" class="col-sm-2 control-label">Nom</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="nom" placeholder="Nom">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="email" class="col-sm-2 control-label">Email</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="email" placeholder="Email">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="ville" class="col-sm-2 control-label">Ville</label>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" id="ville" placeholder="Ville">
+              </div>
+            </div>
+          </form>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+        <button type="button" class="btn btn-primary">Enregistrer</button>
+      </div>
+    </div>
+  </div>
 </div>
+
+
+
 @endsection
--- SRC/resources/views/referent.blade.php
+++ SRC/resources/views/referent.blade.php
@@ -1,9 +1,186 @@
 @extends('app')
 
 @section('content')
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+  	<a class="navbar-brand" href="#">Espace Référent</a>
+ 	<!-- <span class="glyphicon glyphicon-user" aria-hidden="true"></span> -->
+ 	<p class="navbar-text">Icone</p>
+  	<p class="navbar-text">Nom</p>
+  	<p class="navbar-text">Prenom</p>
+  	<p class="navbar-text">Identifiant</p>
+  	<ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Se déconnecter</a></li>
+     </ul>
+  </div>
+</nav>
 
 <br>
-<center><h1>Bienvenue dans l'espace référent!</h1>
-<br>
+<div class="col-md-3">
+
+<legend>Créer une session:</legend>
+<form class="form-inline">
+  <div class="form-group">
+    <div class="input-group">
+      <input type="text" class="form-control" id="exampleInputAmount" placeholder="Ajouter une session">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Ajouter</button>
+</form>
+
+<br><br>
+<legend>Mes sessions:</legend>
+<table class="table table-hover">
+	<tr class="active">
+		<td>Falbala</td>
+		<td>Supprimer</td>
+	</tr>
+	<tr>
+		<td>biblo</td>
+		<td>Supprimer</td>
+	</tr>
+	<tr>
+		<td>Tralala</td>
+		<td>Supprimer</td>
+	</tr>
+	<tr>
+		<td>Redmin</td>
+		<td>Supprimer</td>
+	</tr>
+	<tr>
+		<td>Vive arch linux</td>
+		<td>Supprimer</td>
+	</tr>
+</table>
+
+</div>
+
+
+<div class="col-md-9">
+<legend>Falbala: (Activer ou Désactiver la session)</legend>
+
+<div class="col-md-9">
+
+<span style="float: right">Sélectionner tout -- Annuler sélection</span><br>
+<div class="panel panel-default">
+  <div class="panel-body">
+    
+
+  <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+  <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+  <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+    <div class="col-xs-4 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="/pictures/holder.jpg">
+    </a>
+  </div>
+
+  <button style="float: right" class="btn btn-primary">Enregistrer</button>
+  </div>
+</div>
+
+</div>
+
+
+<div class="col-md-3">
+
+<legend>Ajouter un filtre:</legend>
+<h4>Par critère:</h4>
+<select class="form-control">
+  <option selected="selected">Selectionner catégorie</option>
+  <option>1</option>
+  <option>2</option>
+</select>
+
+<select class="form-control">
+  <option selected="selected">Tous les éléments</option>
+  <option>1</option>
+  <option>2</option>
+</select>
+<button style="margin-top: 5px" class="btn btn-primary">Ajouter</button>
+<br><br>
+
+<h4>Par mot clé:</h4>
+<form class="form-inline">
+  <div class="col-sm-8 form-group">
+    <div class="input-group">
+      <input type="text" class="form-control" id="exampleInputAmount" placeholder="Par mot clé">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Ajouter</button>
+
+<br><br>
+<legend>Mes filtres</legend>
+
+<table class="table table-hover">
+	<tr>
+		<td>Montagne: noire</td>
+		<td>Supprimer</td>
+	</tr>
+	<tr>
+		<td>couleur: bleu</td>
+		<td>Supprimer</td>
+	</tr>
+	<tr>
+		<td>âge: 10 ans</td>
+		<td>Supprimer</td>
+	</tr>
+</table>
+</div>
+
+</div>
+
 
 @endsection
