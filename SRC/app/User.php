fed82155dcf80dcb4a5ede8b55ad8ed94b428fe2
Malibu <matthieu-p93@live.fr>
2015-02-18 18:13:47 +0100

    update

--- SRC/app/User.php
+++ SRC/app/User.php
@@ -1,13 +1,33 @@
 <?php namespace App;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Auth;
 
-class User extends Model {
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+    use Authenticatable, CanResetPassword;
 
 	protected $table = 'users';
 
-	public $timestamps = false;
+	//public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password'];
+
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 
 	public function scopeCurrent($query)
     {
