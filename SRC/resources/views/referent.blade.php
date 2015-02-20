776940a70a8b6690db88ec84b8b55fddc0c836d9
Malibu <matthieu-p93@live.fr>
2015-02-20 12:49:51 +0100

    modified:   SRC/public/css/referent.css 	modified:   SRC/resources/views/referent.blade.php

--- SRC/resources/views/referent.blade.php
+++ SRC/resources/views/referent.blade.php
@@ -4,7 +4,7 @@
 <link href="css/referent.css" rel="stylesheet" type="text/css"/>
 <br>
 <!-- Navbar left -->
-<div class="col-md-3 listg">
+<div class="col-md-2 listg">
 	<legend>Créer une liste oeuvre:</legend>
 	<form class="form-inline" method="POST" role="form" action="addListeOeuvre">
   		<input type="hidden" name="idUser" value="{{ $me->id }}">
@@ -13,38 +13,38 @@
     		<div class="input-group">
       			<input type="text" class="form-control" required="required" name="name" placeholder="Ajouter une liste d'oeuvre">
     		</div>
- 		</div>
+ 		  </div>
   		<button type="submit" class="btn btn-primary">Ajouter</button>
 	</form>
 	<br><br>
 	<legend>Mes listes d'oeuvres:</legend>
 	<table class="table table-hover">
-	<caption>Nom  | Activer | Supprimer</caption>
- 	@foreach ($listeoeuvres as $listeoeuvre)
-		<tr class="active listeoeuvre">
-    		<form method="POST" role="form" action="deleteListeOeuvre">
-    			<input type="hidden" name="idUser" value="{{ $me->id }}">
-    			<input type="hidden" class="idListeOeuvre" name="idListeOeuvre" value="{{ $listeoeuvre->id }}">
-    			<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-				<td>{{$listeoeuvre->nom}}</td>
-				<td>
-					<label class="ios7-switch">
-    				<input type="checkbox" checked>
-    				<span></span>
-    				</label>
-    			</td>
-				<td>
-					<button type="submit" class="btn btn-sm btn-danger">
-					<span class="glyphicon glyphicon-trash"></span></a>
-				</td>
-			</form>
-		</tr>
-	@endforeach
+  	<caption>Nom  | Activer | Supprimer</caption>
+   	@foreach ($listeoeuvres as $listeoeuvre)
+  		<tr class="active listeoeuvre">
+      	<form method="POST" role="form" action="deleteListeOeuvre">
+      		<input type="hidden" name="idUser" value="{{ $me->id }}">
+      		<input type="hidden" class="idListeOeuvre" name="idListeOeuvre" value="{{ $listeoeuvre->id }}">
+      		<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+  				<td>{{$listeoeuvre->nom}}</td>
+  				<td>
+  					<label class="ios7-switch">
+      				<input type="checkbox" checked>
+      				<span></span>
+      			</label>
+      		</td>
+  				<td>
+  					<button type="submit" class="btn btn-sm btn-danger">
+  					<span class="glyphicon glyphicon-trash"></span>
+  				</td>
+  			</form>
+  		</tr>
+  	@endforeach
 	</table>
 </div>
 
 
-<div class="col-md-8 princ">
+<div class="col-md-9 princ">
 	<!-- switch a rajouté -->
   <legend>Falbala: </legend>
 
@@ -53,53 +53,51 @@
       <li role="presentation" class="active"><a href="#">Ma sélection</a></li>
       <li role="presentation"><a href="#">Ajouter des images</a></li>
     </ul>
-    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <br>
+
+    <br><br><br><br><br><br><br><br><br><br><br>
   </div>
 
   <div class="col-md-3">
-  <legend>Mes jeux associés</legend>
+    <legend>Mes jeux associés</legend>
   
     <div class="checkbox">
     	<label class="ios7-switch">
     		<input type="checkbox" checked>
     		<span></span>
     		Puzzle Game
-		</label>
+		  </label>
 
     	<label class="ios7-switch">
     		<input type="checkbox" >
     		<span></span>
     		Jeu du pendu
-		</label>
+		  </label>
 
     	<label class="ios7-switch">
     		<input type="checkbox" >
     		<span></span>
     		Assassin's creed
-		</label>
-    </div>
-    
+		  </label>
+    </div>  
   </div>
   <br><br><br>
 </div>
 
 
-<div class="col-md-8 listRecherche">
-
-    <div class="col-md-9">
-    	
-      <span style="float: right"><a href="">Sélectionner tout</a> -- <a href="">Annuler sélection</a></span><br>
-      <div class="panel panel-default">
-        <div class="panel-body" id="oeuvrePic">
+<!-- TAKE ALL THAT'S INSIDE THIS DIV FOR "MY SELECTION" -->
+<div class="col-md-7 princ" id="selection">
+	
+  <span style="float: right"><a href="">Sélectionner tout</a> -- <a href="">Annuler sélection</a></span><br>
+    <div class="panel-body" id="oeuvrePic">
       <!-- // TODO -->
-        </div>
-      </div>
-      <button style="float: right" class="btn btn-primary" id="enregistrer">Enregistrer</button>
     </div>
+  <button style="float: right" class="btn btn-primary" id="enregistrer">Enregistrer</button>
 </div>
 
 
-<div class="col-md-8 filtred">
+<!-- TAKE ALL THAT'S INSIDE THIS DIV FOR "ADD PICTURES" -->
+<div class="col-md-7 princ" id="addpicture">
 	<div>
     	<legend>Critères</legend>
     	<select class="form-control comboSearch">
@@ -138,7 +136,31 @@
     		</tr>
   		</table>
   	</form>
-  	</div>
+  </div>
+
+  <span style="float: right"><a href="">Sélectionner tout</a> -- <a href="">Annuler sélection</a></span><br>
+  <div class="panel panel-default">
+    <div class="panel-body" id="oeuvrePic">
+
+      <div class="col-xs-4 col-md-3">
+        <a href="#" class="thumbnail">
+        <img src="http://www.augustins.org/documents/10180/156407/1"/>
+        </a>
+      </div>
+      <div class="col-xs-4 col-md-3">
+        <a href="#" class="thumbnail">
+        <img src="http://www.augustins.org/documents/10180/156407/1"/>
+        </a>
+      </div>
+      <div class="col-xs-4 col-md-3">
+        <a href="#" class="thumbnail">
+        <img src="http://www.augustins.org/documents/10180/156407/1"/>
+        </a>
+      </div>
+
+    </div>
+  </div>
+  <button style="float: right" class="btn btn-primary" id="enregistrer">Enregistrer</button>
 </div>
 
 @endsection
