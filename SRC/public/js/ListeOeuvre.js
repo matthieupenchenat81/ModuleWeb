3a980e247b8eb61e165f04b182ce24271d1b4331
DonRenando <renan.husson@hotmail.fr>
2015-03-03 12:05:09 +0100

    ajax

--- SRC/public/js/ListeOeuvre.js
+++ SRC/public/js/ListeOeuvre.js
@@ -5,27 +5,24 @@ $('.listeoeuvre').click(function(event) {
     $(this).addClass("active");
     $('#oeuvrePic').empty();
     url = "/showListOeuvres/" + $(this).children('.idListeOeuvre').val();
-    $.get(url, function( data ) {
+
+
+    var req = new XMLHttpRequest();
+    req.onload = function(e) {
+              var data  = req.reponse;
         if (data.length == 0 )
+
             $("#oeuvrePic").append("Aucune Oeuvre");
         data.forEach( function(el) {
             $("#oeuvrePic").append('<div class="col-xs-4 col-md-3">'
             +'<a href="#" class="thumbnail">'
             +'<img src="http://www.augustins.org/documents/10180/156407/' + el.urlPhoto + '"/>'
             +'</a></div>');
-        })
-    }, "json" )
-    
-    .fail(function() {
-        $("#oeuvrePic").append('<div class="alert alert-danger">'
-        +'<strong>Oouups!</strong> Il y a un problème.<br><br>'
-        +'<ul>'
-        +'<li>Erreur lors de la récupération</li>'
-        +'</ul>'
-        +'</div>'
-        );
-
     });
+    req.open('GET', url, true);
+    req.send();
+
+
 });
 
 
