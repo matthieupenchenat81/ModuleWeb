b655411c09059b75b651e1c3e8047607b1fb2b8f
sidya82 <quentin.rouland.82@gmail.com>
2015-03-05 20:28:28 +0100

    modifié :         SRC/resources/views/emails/password.blade.php

--- SRC/resources/views/emails/password.blade.php
+++ SRC/resources/views/emails/password.blade.php
@@ -1,4 +1,4 @@
 Bonjour,
 Cliquer ici pour réinitialiser votre mot de passe : {{ url('password/reset/'.$token) }}
-Pour vous logguer : {{ url('login) }}
+Pour vous logguer : {{ url('login') }}
 Merci et à bientôt!
