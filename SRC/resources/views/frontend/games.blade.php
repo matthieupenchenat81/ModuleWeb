08bc1a56e5af13e753072d3a81364fb8a2d75cd5
alexandre-pereira <alexandre.pereira.it@gmail.com>
2015-03-18 11:44:19 +0100

    wOOP

--- SRC/resources/views/frontend/games.blade.php
+++ SRC/resources/views/frontend/games.blade.php
@@ -4,11 +4,11 @@
 @section('content')
     <div style="text-align:center">
         
-       <img id="imgRef" src="{{ $ref -> image }}"><br>
+        <div id="imgRef" style="background-image:url('{{ $ref -> image }}');"></div><br>
         
         
         <div class="game" onclick="location.href='{{ URL::to('memo') }}'"><img src="imgs/memo/memo2.png"></div>
-       <div class="game" onclick="location.href='{{ URL::to('puzzle') }}'"><img src="imgs/puzzle/puzzle.png"></div> 
+        <div class="game" onclick="location.href='{{ URL::to('puzzle') }}'"><img src="imgs/puzzle/puzzle.png"></div> 
         
     </div>
 @endsection
