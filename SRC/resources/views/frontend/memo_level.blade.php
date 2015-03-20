31a37585b982499ed380f74fcd7f64c0e6580d45
Jerome Morjon <liljerome@hotmail.fr>
2015-03-20 21:36:50 +0100

    edit code
    serieux les gars faites attentions quand vous fermez les balises etc
    plein de block incomplet…

--- SRC/resources/views/frontend/memo_level.blade.php
+++ SRC/resources/views/frontend/memo_level.blade.php
@@ -3,26 +3,23 @@
 @section('content')
 
 <div class="referents">
-<div class="referent game"
+	<div class="referent game"
          onclick='location.href = "{{ URL::to('memo') }}";'
          style="background:url('{{ URL::to('imgs/memo/memo2.png') }}');  background-repeat:no-repeat; background-size: contain; border: none; margin-top: 10%;">
     </div>
-
     <br>
     <button class="level" onclick="location.href='{{ URL::to('memo/jouer/1') }}'"><span style="color:gold;"><span class="icon-star-full"></span></span><span class="icon-star-full"></span><span class="icon-star-full"></span></button>
 
-<button class="level" onclick="location.href='{{ URL::to('memo/jouer/2') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span></span><span class="icon-star-full"></span></button>
+	<button class="level" onclick="location.href='{{ URL::to('memo/jouer/2') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span></span><span class="icon-star-full"></span></button>
 
-<button class="level" onclick="location.href='{{ URL::to('memo/jouer/3') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span><span class="icon-star-full"></span></span></button><br>
-
-
-
+	<button class="level" onclick="location.href='{{ URL::to('memo/jouer/3') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span><span class="icon-star-full"></span></span></button><br>
 
 </div>
 
-<div style="position: relative; left: 0; bottom:40%; padding-left:1%; ">
-    <a href="/"><img style="height: 80px;" src="{{ URL::to('imgs/previouspage.png') }}"></a>
+<div style="position:relative; left: 0; bottom: 400px; padding-left:1%;">
+    <a href="/"><img style="height: 80px;" src="{{ URL::to('imgs/previouspage.png') }}" /></a>
 </div>
+@endsection
 
 @section('page-css')
 <link href="{{ URL::to('css/fonts/style.css') }}" rel="stylesheet" type="text/css"/>
--- SRC/resources/views/frontend/puzzle_level.blade.php
+++ SRC/resources/views/frontend/puzzle_level.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <div class="abso referents">
-<div class="game active"
+	<div class="game active"
          onclick='location.href = "{{ URL::to('puzzle') }}";'
          style="background:url('{{ URL::to('imgs/puzzle/puzzle.png') }}'); background-repeat:no-repeat; background-size: contain; border: none; margin-top: 10%;">
     </div>
-<br>
+	<br>
     <button class="level" onclick="location.href='{{ URL::to('puzzle/jouer/1') }}'">
     	<span style="color:gold;">
     		<span class="icon-star-full"></span>
@@ -15,17 +15,16 @@
     	<span class="icon-star-full"></span>
     	<span class="icon-star-full"></span>
     </button>
-    
 
-    
     <button  class="level" onclick="location.href='{{ URL::to('puzzle/jouer/2') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span></span><span class="icon-star-full"></span></button>
     
-<button class="level" onclick="location.href='{{ URL::to('puzzle/jouer/3') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span><span class="icon-star-full"></span></span></button>
+	<button class="level" onclick="location.href='{{ URL::to('puzzle/jouer/3') }}'"><span style="color:gold;"><span class="icon-star-full"></span><span class="icon-star-full"></span><span class="icon-star-full"></span></span></button>
 </div>
 
-<div style="position: relative; left: 0; bottom:40%; padding-left:1%; ">
-    <a href="/"><img style="height: 80px;" src="{{ URL::to('imgs/previouspage.png') }}"></a>
+<div style="position: relative; left: 0; bottom: 400px; padding-left:1%; ">
+    <a href="/"><img style="height: 80px;" src="{{ URL::to('imgs/previouspage.png') }}"/></a>
 </div>
+@endsection
 
 @section('page-css')
 <link href="{{ URL::to('css/fonts/style.css') }}" rel="stylesheet" type="text/css"/>
