5f3e3d37086313f4f76ebad89291af2d8bd9a40f
Jerome Morjon <liljerome@hotmail.fr>
2015-03-20 19:39:46 +0100

    maj response

--- SRC/app/Http/Controllers/GameController.php
+++ SRC/app/Http/Controllers/GameController.php
@@ -97,12 +97,9 @@ class GameController extends Controller {
             default:
                 break;
         }
-<<<<<<< HEAD
-        Response::make('records')->withCookie(Cookie::forever('trophee', $values));
-=======
+
         
         $response->withCookie(Cookie::forever('trophee', $values));
->>>>>>> 550ca526b9ba7c2d56b4b5096b3bce0d7e567414
     }
 
 }
