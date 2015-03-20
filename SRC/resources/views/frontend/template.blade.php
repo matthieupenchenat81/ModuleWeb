c5ffaded2f241ba298b01492defc00574f85798d
sidya82 <quentin.rouland.82@gmail.com>
2015-03-20 19:46:10 +0100

    nouveau fichier : SRC/public/imgs/favicon.png 	modifié :         SRC/resources/views/backend/template.blade.php 	modifié :         SRC/resources/views/frontend/template.blade.php

--- SRC/resources/views/frontend/template.blade.php
+++ SRC/resources/views/frontend/template.blade.php
@@ -6,7 +6,7 @@
     <title>Les Jeux du Musée</title>
     <link href="http://fonts.googleapis.com/css?family=Oswald:700,300" rel="stylesheet" type="text/css">
     <link href="{{ URL::to('css/frontend.css') }}" rel="stylesheet" type="text/css"/>
-    <link rel="icon" type="image/ico" href="{{ URL::to('imgs/favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ URL::to('imgs/favicon.png') }}">
     @yield('page-css')
 </head>
 <body>
